01bfd08f2db81af67f895473a6476864e9ee65dd
Paul <paul.huynh3@gmail.com>
2017-09-14 17:05:39 -0400

    add tab bar and images

---
+++ UIGestureRecognizers/UIGestureRecognizers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIGestureRecognizers
+//
+//  Created by Paul on 2017-09-14.
+//  Copyright © 2017 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIGestureRecognizers/UIGestureRecognizers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIGestureRecognizers
+//
+//  Created by Paul on 2017-09-14.
+//  Copyright © 2017 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
